d526b3e4139343937f69fd3089d84e7c8e3fc6cc
Joshua Rubin <joshuarubin@users.noreply.github.com>
2014-10-28 18:14:26 -0600

    Create prompt.plugin.zsh

---
+++ prompt.plugin.zsh
@@ -0,0 +1,12 @@
+#
+# Loads prompt themes.
+#
+# Authors:
+#   Sorin Ionescu <sorin.ionescu@gmail.com>
+#
+
+# Load and execute the prompt theming system.
+autoload -Uz promptinit && promptinit
+
+# Load the prompt theme.
+prompt joshuarubin
