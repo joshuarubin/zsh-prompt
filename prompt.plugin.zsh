3ad8018bfd04da64699ceeda0d7cb2aab5883dd1
Joshua Rubin <jrubin@zvelo.com>
2014-10-28 18:17:36 -0600

    add themes
    Signed-off-by: Joshua Rubin <jrubin@zvelo.com>

--- prompt.plugin.zsh
+++ prompt.plugin.zsh
@@ -5,6 +5,10 @@
 #   Sorin Ionescu <sorin.ionescu@gmail.com>
 #
 
+local fdir=$0:A:h/functions
+fpath+=$fdir
+autoload -Uz $fdir/*(:t)
+
 # Load and execute the prompt theming system.
 autoload -Uz promptinit && promptinit
 
