ef4abfef3db9670e9a4e194a35caee7a2b63afa8
Joshua Rubin <jrubin@zvelo.com>
2015-10-13 18:11:15 -0600

    add a bunch more prompts

--- prompt.plugin.zsh
+++ prompt.plugin.zsh
@@ -12,5 +12,10 @@ autoload -Uz $fdir/*(:t)
 # Load and execute the prompt theming system.
 autoload -Uz promptinit && promptinit
 
-# Load the prompt theme.
-prompt nicoulaj
+zstyle -a ':prezto:module:prompt' theme 'prompt_argv'
+if [[ "$TERM" == (dumb|linux|*bsd*) ]] || (( $#prompt_argv < 1 )); then
+  prompt 'off'
+else
+  prompt "$prompt_argv[@]"
+fi
+unset prompt_argv
